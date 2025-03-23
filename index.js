8a2bc82e9857a929c5fdb69c22153fe0bdbfe2a9
fvop <141462158+fvop@users.noreply.github.com>
2025-03-24 01:34:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
 const wa = require('@open-wa/wa-automate');
+
+const sessionPath = './session.data.json';
+
+let sessionData = null;
+if (fs.existsSync(sessionPath)) {
+  sessionData = require(sessionPath);
+}
 
 wa.create({
   sessionId: "WHATSAPP_SESSION",
+  sessionData,
   multiDevice: true,
   authTimeout: 60,
   blockCrashLogs: true,
@@ -9,7 +18,10 @@ wa.create({
   headless: true,
   logConsole: false,
   qrTimeout: 0,
-}).then(client => start(client));
+}).then(client => {
+  fs.writeFileSync(sessionPath, JSON.stringify(client.getSessionData()));
+  start(client);
+});
 
 function start(client) {
   client.onMessage(async message => {
